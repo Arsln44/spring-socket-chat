b4b0b506ec0e3977615fedd28acda7176a80a89e
Burak Alp Arslan <arslnburak2001@gmail.com>
2025-11-25 00:37:42 +0300

    Initial setup

---
+++ src/main/java/sphynx/springsocketchat/SpringSocketChatApplication.java
@@ -0,0 +1,13 @@
+package sphynx.springsocketchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSocketChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSocketChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sphynx/springsocketchat/SpringSocketChatApplicationTests.java
@@ -0,0 +1,13 @@
+package sphynx.springsocketchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSocketChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
