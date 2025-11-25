9c50bdbd53ea270df72cd3191237fc7e117169f3
Burak Alp Arslan <arslnburak2001@gmail.com>
2025-11-25 04:01:27 +0300

    feat: Add ChatController and update ChatMessage model

---
+++ src/main/java/sphynx/springsocketchat/controller/ChatController.java
@@ -0,0 +1,16 @@
+package sphynx.springsocketchat.controller;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import sphynx.springsocketchat.model.ChatMessage;
+
+@Controller
+public class ChatController {
+
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/public")
+    public ChatMessage sendMessage(ChatMessage chatMessage){
+        return chatMessage;
+    }
+}
--- src/main/java/sphynx/springsocketchat/model/ChatMessage.java
+++ src/main/java/sphynx/springsocketchat/model/ChatMessage.java
@@ -3,4 +3,28 @@ package sphynx.springsocketchat.model;
 public class ChatMessage {
     String message;
     String sender;
+
+    public ChatMessage() {
+    }
+
+    public ChatMessage(String message, String sender) {
+        this.message = message;
+        this.sender = sender;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
 }
